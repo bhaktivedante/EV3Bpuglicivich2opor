f06379c3c24c1483a06da44cdd88d67f6cb9a3a5
bhaktivedante <bhaktivedante.puglicivich@inacapmail.cl>
2024-12-15 22:51:55 -0300

    Agregar archivos de proyecto.

---
+++ RegistroEstudiantes.AppMovil/Vistas/CrearEstudiante.xaml.cs
@@ -0,0 +1,48 @@
+using Firebase.Database;
+using RegistroEstudiantes.Modelos.Modelos;
+namespace RegistroEstudiantes.AppMovil.Vistas;
+
+public partial class CrearEstudiante : ContentPage
+{
+    FirebaseClient client = new FirebaseClient("https://registroestudiantes-7ec8c-default-rtdb.firebaseio.com/");
+
+    public List<Curso> Cursos { get; set; }
+    public CrearEstudiante()
+    {
+        InitializeComponent();
+        ListarCursos();
+        BindingContext = this;
+    }
+
+    private void ListarCursos()
+    {
+        var cursos = client.Child("Cursos").OnceAsync<Curso>();
+        Cursos = cursos.Result.Select(x => x.Object).ToList();
+    }
+
+    private async void guardarButton_Clicked(object sender, EventArgs e)
+    {
+        Curso curso = cursoPicker.SelectedItem as Curso;
+
+        var estudiante = new Estudiante
+        {
+            Nombre = NombreEntry.Text,
+            Apellido = ApellidoEntry.Text,
+            CorreoElectronico = CorreoElectronicoEntry.Text,
+            Edad = int.Parse(EdadEntry.Text),
+            Curso = curso
+        };
+
+        try
+        {
+            await client.Child("Estudiantes").PostAsync(estudiante);
+            await DisplayAlert("Éxito", $"El Estudiante {estudiante.Nombre} {estudiante.Apellido} fue guardado correctamente", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
+
+    }
+}
---
+++ RegistroEstudiantes.AppMovil/Vistas/ListarEstudiantes.xaml.cs
@@ -0,0 +1,48 @@
+using Firebase.Database;
+using RegistroEstudiantes.Modelos.Modelos;
+using System.Collections.ObjectModel;
+
+namespace RegistroEstudiantes.AppMovil.Vistas;
+
+public partial class ListarEstudiantes : ContentPage
+{
+    FirebaseClient client = new FirebaseClient("https://registroestudiantes-7ec8c-default-rtdb.firebaseio.com/");
+    public ObservableCollection<Estudiante> Lista { get; set; } = new ObservableCollection<Estudiante>();
+    public ListarEstudiantes()
+    {
+        InitializeComponent();
+        BindingContext = this;
+        CargarLista();
+    }
+
+    private void CargarLista()
+    {
+        client.Child("Estudiantes").AsObservable<Estudiante>().Subscribe((estudiante) =>
+        {
+            if (estudiante != null)
+            {
+                Lista.Add(estudiante.Object);
+            }
+
+        });
+    }
+
+    private void filtroSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string filtro = filtroSearchBar.Text.ToUpper();
+
+        if (filtro.Length > 0)
+        {
+            listaCollection.ItemsSource = Lista.Where(x => x.NombreCompleto.ToUpper().Contains(filtro));
+        }
+        else
+        {
+            listaCollection.ItemsSource = Lista;
+        }
+    }
+
+    private async void NuevoEstudianteBoton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new CrearEstudiante());
+    }
+}
